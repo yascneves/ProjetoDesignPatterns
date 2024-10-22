586b9861eeda595a04404181051e428a9d168edf
Yasmim <yascneves@hotmail.com>
2024-10-22 19:44:53 -0300

    renomeio da main para que o arquivo rode!

--- ProjetoAntigo/Main2.java
+++ ProjetoAntigo/Main2.java
@@ -12,7 +12,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 // O caixa de uma loja possui uma aplicação cujo banco de dados está armazenado na nuvem.
 // Em caso de problemas de acesso (sem conexão internet, por ex.) o sistema possui 
 // um recurso de CONTINGÊNCIA que  permite a loja  funcionar  mesmo sem o acesso a 
-// qualquer tipo de rede. O estratégia consiste em:
+// qualquer tipo de rede. A estratégia consiste em:
 // 1º Tentar acessar o banco de dados na nuvem (pela conexão via internet);
 // 2º Na indisponibilidade da internet, tentar acessar o banco de dados da rede local da loja;
 // 3º Na indisponibilidade da LAN, gravar os dados no banco de dados na máquina local.
